7a430fa6634d899e857725cc477a0a68b74c5cad
GrzegorzWK <grzegorzwalesiak@gmail.com>
2023-03-29 21:17:56 +0200

    counter works, infos

--- script_new.js
+++ script_new.js
@@ -1,13 +1,16 @@
-const content = document.querySelector("h2.text");
-// const button_start = document.querySelector(".btn_play");
+const content_result = document.querySelector("h2.result");
+const content_action = document.querySelector("h2.action");
+const content_standings = document.querySelector("h2.standings");
+
 const button_reset = document.querySelector(".btn_reset");
 
 const button_rock = document.querySelector(".rock");
 const button_paper = document.querySelector(".paper");
 const button_scissors = document.querySelector(".scissors");
 
-content.innerHTML = "Welcome to our game traveler!";
-const game_counter = 0;
+content_result.innerHTML = "Welcome to our game traveler!";
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const computerChoice = Math.random() * 10;
@@ -20,12 +23,37 @@ function getComputerChoice() {
   }
 }
 
+function round(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return (result = "Tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return (result = "You win!");
+  } else {
+    return (result = "You lose!");
+  }
+}
+
 function start_game(playerSelection) {
-  return console.log(playerSelection);
+  const computerSelection = getComputerChoice();
+
+  if (playerScore >= 5) {
+    content_result.innerHTML = `You win the game! You Rock!`;
+  } else if (computerScore >= 5) {
+    content_result.innerHTML = `You lose! You Suck!`;
+  } else {
+    let result = round(playerSelection, computerSelection);
+    content_result.innerHTML = `${result}`;
+    content_standings.innerHTML = `Player score: ${playerScore} Computer score: ${computerScore}`;
+  }
 }
 
 function reset_game() {
-  return (game_counter = 0);
+  playerScore = 0;
+  computerScore = 0;
 }
 
 button_rock.addEventListener("click", () => start_game("rock"));
